b5d12078337fef40835f7c8fc976182352845728
Jonathan Curtis <06jcurtis@gmail.com>
2024-06-02 08:10:46 -0400

    cleaned up email stuff

--- src/routes/api/subscribe/+server.ts
+++ src/routes/api/subscribe/+server.ts
@@ -5,16 +5,13 @@ import { RESEND_API_KEY } from '$env/static/private';
 const resend = new Resend(RESEND_API_KEY);
 
 export const POST: RequestHandler = async ({ request }) => {
-  console.log("hello world")
-  console.log('Request', request);
-  console.log('API KEY', RESEND_API_KEY);
   const { email } = await request.json();
 
   const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
+    from: 'PrintPoly <onboarding@resend.dev>',
     to: [email],
-    subject: 'Hello World',
-    html: '<strong>It works!</strong>',
+    subject: "PrintPoly Email Subscription Confirmation",
+    html: "<strong>Hey there!</strong> Thanks for signing up for beta notifications.",
   });
 
   if (error) {
